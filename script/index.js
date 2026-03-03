3c59e6929db1a6dd23c8ef0ad23c9c6e7addf8df
mdarifuzzaman0916-dev <mdarifuzzaman0916@gmail.com>
2026-03-03 11:54:03 +0600

    added lesson word

--- script/index.js
+++ script/index.js
@@ -4,6 +4,28 @@ const load = () => {
         .then((data) => displayLesson(data.data))
 }
 
+const loadWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}
+`
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displayWord(data.data))
+}
+
+const displayWord = (words) => {
+
+    const wordContainer = document.getElementById('word-container')
+    wordContainer.innerHTML = ""
+
+    words.forEach(word => {
+      const card = document.createElement('div')
+      card.innerHTML=`
+      <p>akash</p>
+      `
+       wordContainer.append(card)
+    });
+}
+
 const displayLesson = (lessons) => {
     const levelContainer = document.getElementById('level-container')
     levelContainer.innerHTML = ""
@@ -13,12 +35,12 @@ const displayLesson = (lessons) => {
         const div = document.createElement('div')
 
         div.innerHTML = `
-           <button class="btn btn-outline btn-primary">
+           <button onclick="loadWord(${lesson.level_no})" class="btn btn-outline btn-primary">
            <i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no}</button>
         `;
         levelContainer.append(div)
 
-        
+
     }
 }
 load()
